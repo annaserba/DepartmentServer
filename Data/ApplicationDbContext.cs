33407a6c5ab49eac2ee9321762b9f855b02cce88
annaserba <60755945+annaserba@users.noreply.github.com>
2020-05-31 14:10:01 +0300

    Add project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DepartmentServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DepartmentServer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
