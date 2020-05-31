afce83fd2c3851cfaf31c4d13d71bb1aa1afbb2f
annaserba <60755945+annaserba@users.noreply.github.com>
2020-05-31 16:35:11 +0300

    add Data

---
+++ Migrations/20200531132837_Data.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DepartmentServer.Migrations
+{
+    public partial class Data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO ""Departments""(""Title"")
+                VALUES('IT department'), ('Marketing'); ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
